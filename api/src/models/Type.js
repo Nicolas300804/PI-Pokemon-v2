e151b2edd6893d06119432f8d90efc15da64d346
Nicolas González Rodríguez <nicogr300804@gmail.com>
2023-02-20 10:39:06 -0500

    Back terminado

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -6,7 +6,6 @@ module.exports = (sequelize) => {
   sequelize.define('type', {
     id:{
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 // Importar todos los routers;
 const RoutePKM = require ("./pokemons")
+const TypesPKM = require ("./types")
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 router.use("/pokemons", RoutePKM)
+router.use("/types", TypesPKM) 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
